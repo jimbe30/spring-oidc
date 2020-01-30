915cacd31d56551f41393cd27413b41da78f87dc
JMB <jean-marc.brun@cegedim-activ.com>
2020-01-30 17:53:14 +0100

    Customisation URL de redirection (callback) pour Google

--- spring-security-demo/src/main/java/jmb/example/security/WebSecurityConfig.java
+++ spring-security-demo/src/main/java/jmb/example/security/WebSecurityConfig.java
@@ -1,15 +1,19 @@
 package jmb.example.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig 
 extends WebSecurityConfigurerAdapter 
 {
+	
 //	@Override
 //	protected void configure(HttpSecurity http) throws Exception {
 //		http
@@ -17,25 +21,35 @@ extends WebSecurityConfigurerAdapter
 //				.antMatchers("/", "/accueil").permitAll()
 //				.anyRequest().authenticated()
 //				.and()
-//			.formLogin()
-//				.loginPage("/login")
-//				.permitAll()
+//			.oauth2Login()
 //				.and()
 //			.logout()
 //				.permitAll();
 //	}
 	
+	@Autowired
+	ClientRegistrationRepository clientRegistrationRepository;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		
+		ClientRegistration googleRegistration = clientRegistrationRepository.findByRegistrationId("google");
+		System.out.println("google.redirectUriTemplate: " + googleRegistration.getRedirectUriTemplate());
+		
 		http
 			.authorizeRequests()
 				.antMatchers("/", "/accueil").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.oauth2Login()
+				.redirectionEndpoint().baseUri("/oidcAuth").and()	// doit matcher avec redirectUriTemplate du ClientRegistration
+				// (propriete redirect-uri dans application.properties)			
 				.and()
 			.logout()
 				.permitAll();
 	}
+	
+	
+
 
 }
