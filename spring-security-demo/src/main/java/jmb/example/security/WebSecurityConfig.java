2be3ac3b84083b8d663548f34eb97683bd72e9b9
JMB <jean-marc.brun@cegedim-activ.com>
2020-02-17 18:26:16 +0100

    OIDC: Personnnalisation des rôles (GrantedAuthority) via OAuth2UserService + Serveur ressources: config STATELESS avec authentification par JWT dans header (Authorization: Bearer)

--- spring-security-demo/src/main/java/jmb/example/security/WebSecurityConfig.java
+++ spring-security-demo/src/main/java/jmb/example/security/WebSecurityConfig.java
@@ -1,25 +1,43 @@
 package jmb.example.security;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
+	
 
 	@Bean
 	/**
 	 * Configuration des infos de référencement de l'application auprès des différents fournisseurs d'identité.
-	 * 
 	 * @return
 	 */
 	public ClientRegistrationRepository clientRegistrationRepository() {
@@ -32,9 +50,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.redirectUriTemplate(redirectUri)
 				.build();
 		
-		String keycloakBaseUri = "http://localhost:8080/auth/realms/OIDC-demo/protocol/openid-connect"; 
+		String keycloakBaseUri = "http://localhost:8180/auth/realms/OIDC-demo/protocol/openid-connect"; 
 		ClientRegistration keycloakRegistration = ClientRegistration.withRegistrationId("keycloak")
 				.authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+				.scope("profile", "email", "openid")
 				.clientId("oidcDemoApp")
 				.clientSecret("f24fa57d-a8bc-4993-8a3d-afddbf3c6903")
 				.redirectUriTemplate(redirectUri)
@@ -48,26 +67,107 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return new InMemoryClientRegistrationRepository(googleRegistration, keycloakRegistration);
 	}
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Configuration @Order(1)
+	public static class JwtBearerHttpConfig extends WebSecurityConfigurerAdapter {
 		
-		http
-			.authorizeRequests()
-				.antMatchers("/*", "/resources/**", "/accueil").permitAll()
-				.antMatchers("/secure/").hasRole("USER")
-				.anyRequest().authenticated()
-				.and()
-			.oauth2Login()
-				.redirectionEndpoint().baseUri("/oidcAuth").and()	// doit matcher avec redirectUriTemplate du ClientRegistration
-												// (propriété redirect-uri dans application.properties)			
-				.loginPage("/login.html")
-				.defaultSuccessUrl("/accueil")
-				.and()
-			.logout()
-				.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
-				.logoutSuccessUrl("/login.html")
-				.permitAll();
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			
+			http
+				.requestMatcher(
+					request -> request.getHeader("Authorization") != null && request.getHeader("Authorization").startsWith("Bearer ")
+				)
+				.sessionManagement()
+					.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+					.and()
+				.oauth2ResourceServer()
+					.jwt()
+						.jwkSetUri("http://localhost:8180/auth/realms/OIDC-demo/protocol/openid-connect/certs")
+						.and()
+					.and()
+				.authorizeRequests()
+					.antMatchers("/*", "/accueil").permitAll()
+					.anyRequest().authenticated()
+					.and()
+				.logout()				
+					.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+					.logoutSuccessUrl("/login.html")
+					.permitAll();			
+
+		}
 	}
+	
+	@Configuration @Order(5)
+	public static class OidcHttpConfig extends WebSecurityConfigurerAdapter {
+		
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			
+			http
+				.authorizeRequests()
+					.antMatchers("/*", "/accueil").permitAll()
+					.antMatchers("/secure/ressources/nouvelle").hasRole("USER")
+					.antMatchers("/secure/**").hasRole("ADMIN")
+					.anyRequest().authenticated()
+					.and()
+				.oauth2Login()
+					.redirectionEndpoint().baseUri("/oidcAuth").and()	// doit matcher avec redirectUriTemplate du ClientRegistration
+													// (propriété redirect-uri dans application.properties)			
+					.loginPage("/login.html")
+					.defaultSuccessUrl("/accueil")
+					.userInfoEndpoint()
+						.oidcUserService(oidcUserService())
+						.userService(userService())
+						.and()
+					.and()
+				.logout()
+					.logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+					.logoutSuccessUrl("/login.html")
+					.permitAll();
+		}
+		
+		private OAuth2UserService<OAuth2UserRequest, OAuth2User> userService() {
+			final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();		
+
+	        return (userRequest) -> {
+	            // Delegate to the default implementation for loading a user
+	        	OAuth2User user = delegate.loadUser(userRequest);
+
+	            OAuth2AccessToken accessToken = userRequest.getAccessToken();
+	            Map<String, Object> attributes = new HashMap<>();
+	            attributes.putAll(user.getAttributes());
+	            attributes.computeIfAbsent("idToken", k -> accessToken.getTokenValue());      
+	            
+	            user = new DefaultOAuth2User(user.getAuthorities(), attributes, 
+	            		userRequest.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUserNameAttributeName());
+
+	            return user;
+	        };
+		}
+
+		private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+	        final OidcUserService delegate = new OidcUserService();
+
+	        return (userRequest) -> {
+	            // Delegate to the default implementation for loading a user
+	            OidcUser oidcUser = delegate.loadUser(userRequest);
+
+	            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
+	            mappedAuthorities.addAll(oidcUser.getAuthorities());
+	            
+	            if (oidcUser.getAttribute("profile").equals("admin")) {
+	            	mappedAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+	            }
+	            
+	            System.out.println(oidcUser.getIdToken().getTokenValue());
+
+	            oidcUser = new DefaultOidcUser(mappedAuthorities, oidcUser.getIdToken(), oidcUser.getUserInfo());
+	            return oidcUser;
+	        };
+	    }
+		
+	}
+	
 	
 	
 
