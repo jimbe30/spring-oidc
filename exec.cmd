79f90fbaa30c39bb4fb5348b60d77c7e860f9ac8
fos1e2 <jean-marc.brun@cegedim-activ.com>
2022-02-11 15:20:34 +0100

    chemin vers Git repo

--- exec.cmd
+++ exec.cmd
@@ -1,1 +1,1 @@
-java -Dserver.port=8090 -jar C:\dev_demo\git_repositories\spring-oidc\target\spring-oidc-service.jar "--spring.profiles.active=R7"
+java -Dserver.port=8090 -jar C:\dev_demo\git-repositories\spring-oidc\target\spring-oidc-service.jar "--spring.profiles.active=R7"
